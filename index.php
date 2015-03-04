1b15a156d82227680b718a932d1a25564d0356af
raymundo mejia <raybot11@gmail.com>
2015-03-03 16:46:18 -0800

    working index

--- index.php
+++ index.php
@@ -7,13 +7,14 @@ $db = "login";
 mysql_connect($host,$user,$pass) or die($dberror);
 mysql_select_db($db);
 
-if(isset($_POST[''])){
+if(isset($_POST['username'])){
+
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-	$sql = "SELECT * FROM 'user' WHERE  username='".$user."' AND password='".$pass."' LIMIT 1";
-	$result = mysql_query($sql);
+	$sql = "SELECT * FROM user WHERE  user='".$username."' AND password='".$password."' LIMIT 1";
+	$result = mysql_query($sql) or die($query."<br/><br/>".mysql_error());
 	if(mysql_num_rows($result) == 1){
-	echo "you have logged in";
+	echo "Whats up. You have logged in to your database";
 	exit();
 	}else{
 	echo "INVALID. Please return to the previous page.";
@@ -32,7 +33,7 @@ if(isset($_POST[''])){
 	<div id="Sign-In"> 
 		<fieldset style="width:50%">
 		<legend>LOG-IN HERE</legend> 
-		<form method="POST" action="login.php"> 
+		<form method="POST" action="index.php"> 
 			User <br> 
 			<input type="text" name="username" size="40">
 			<br> Password <br>
